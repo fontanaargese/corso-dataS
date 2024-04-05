7987034bea9566fecb35759841abcea140d6b245
fontanaargese <159629002+fontanaargese@users.noreply.github.com>
2024-04-05 16:54:53 +0200

    Add files via upload

--- sequenza.py
+++ sequenza.py
@@ -1,9 +1,14 @@
+
 def sequenza():
+    #inserisci il numero
     numero=int(input("inserisci un numero: "))
     counter=True
+    #inizalizza sequenza
     seq=[0,1]
     inizio=0
+    #creo ciclo while
     while counter:
+        #creo nuovi elementi per ogni sequenza
         elemento=seq[-1]+seq[-2]
         seq.append(elemento)
         if seq[-1]>numero:
