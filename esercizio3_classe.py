71c3bf6ba0bfd9a6c5c6f23a897f3194e2ccf4df
fontanaargese <159629002+fontanaargese@users.noreply.github.com>
2024-04-08 14:49:53 +0200

    Add files via upload

---
+++ esercizio3_classe.py
@@ -0,0 +1,26 @@
+#Definisco la classe
+class Ristorante:
+    pass
+    #metto anche attributo aperto
+   #all'interno quindi definisco l'init con due attributi: nome e tipo
+    #creo un dizionario
+    def descrivi_ristorante(self): 
+        pass
+        #metto self
+        #metto la funzione del print con la descrizione 
+    
+    def stato_apertura(self):
+        pass
+        #print(self.aperto)
+
+
+    def apri_ristorante(self):
+        pass
+        #if per controllare se è aperto 
+        #se è chiuso cambiare self.aperto  è printare
+
+    def chiudi_ristorante(self):
+        pass
+        #same ma al contrario 
+    
+
